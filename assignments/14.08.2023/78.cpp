b61d981d970ec97802bcdd3295beb171556b7801
Kalash <kalash.s@ahduni.edu.in>
2023-08-14 14:44:42 +0530

    14/8/2023

---
+++ assignments/14.08.2023/78.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=78 lang=cpp
+ *
+ * [78] Subsets
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    void solve(vector<int> nums, vector<int> output, int index, vector<vector<int>> &ans)
+    {
+        if (index >= nums.size())
+        {
+            ans.push_back(output);
+            return;
+        }
+
+        solve(nums, output, index + 1, ans);
+
+        output.push_back(nums[index]);
+        solve(nums, output, index + 1, ans);
+    }
+
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index = 0;
+        solve(nums, output, index, ans);
+        return ans;
+    }
+};
+// @lc code=end
